6b48beaabc9c022a52ba065b91d37a56be7ec920
dominik <c9kerdom@vasvari.org>
2023-09-27 16:27:38 +0200

    darkmode!

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function darkMode(){
-    var root = document.querySelector(':root');
-    var rootStyle = getComputedStyle(root);
-    var mainColor = rootStyle.getPropertyValue('--main-color');
-    root.style.setProperty('--main-color', '#446A37')
-}
-
-
